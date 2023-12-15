f935efcab5d6c978f3038da53db5cfe30f782754
JGuilhermeCoelho <jose.guilherme.oliveira@ccc.ufcg.edu.br>
2023-12-15 00:32:14 -0300

    aprimora jogo forca

--- 3-programacao-orientada-a-objetos/projeto-final-jogo-da-forca/forca.js
+++ 3-programacao-orientada-a-objetos/projeto-final-jogo-da-forca/forca.js
@@ -1,10 +1,12 @@
+const readline = require('readline')
+
 class Match {
   constructor(word, hint) {
-    this.word = word.toLowerCase() // Palavra a ser adivinhada
-    this.hint = hint // Dica para a palavra
-    this.guessedLetters = new Set() // Conjunto de letras adivinhadas
-    this.maxAttempts = 6 // Número máximo de tentativas
-    this.attemptsLeft = this.maxAttempts // Tentativas restantes
+    this.word = word.toLowerCase()
+    this.hint = hint
+    this.guessedLetters = new Set()
+    this.maxAttempts = 6
+    this.attemptsLeft = this.maxAttempts
   }
 
   displayProgress() {
@@ -104,6 +106,33 @@ class GameController {
     this.match = new Match(word, hint)
   }
 
+  askForWordAndHint() {
+    const rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout,
+    })
+
+    rl.question('Digite a palavra para o jogo: ', (word) => {
+      rl.question('Digite a dica para a palavra: ', (hint) => {
+        this.startNewGame(word, hint, 'Jogador1')
+        rl.close()
+        this.displayGameStatus()
+        this.askForAction()
+      })
+    })
+  }
+
+  nextStep() {
+    if (!this.match.isGameOver()) {
+      this.askForAction()
+    } else {
+      const message = this.match.isWordGuessed()
+        ? 'Você ganhou!'
+        : 'Você perdeu!'
+      console.log(`${message} Fim do jogo!`)
+    }
+  }
+
   displayGameStatus() {
     console.log(`Palavra: ${this.match.displayProgress()}`)
     console.log(`Dica: ${this.match.hint}`)
@@ -149,76 +178,43 @@ class GameController {
     this.match.guessWord(word)
     this.updateGame()
   }
-}
 
-function playGame() {
-  const gameController = new GameController()
-
-  // Iniciar um novo jogo com uma palavra e uma dica
-  gameController.startNewGame(
-    'javascript',
-    'Linguagem de programação',
-    'Jogador1'
-  )
-
-  function displayGameInfo() {
-    console.log(`Palavra: ${gameController.match.displayProgress()}`)
-    console.log(
-      `Quantidade de letras: ${gameController.match.getWord().length}`
-    )
-    console.log(`Dica: ${gameController.match.hint}`)
-    console.log(
-      `Tentativas restantes: ${gameController.match.getAttemptsLeft()}`
-    )
-    console.log(`Pontuação atual: ${gameController.player.getScore()}`)
-  }
-
-  function askForAction() {
-    displayGameInfo()
-    let readline = require('readline').createInterface({
+  askForAction() {
+    const rl = readline.createInterface({
       input: process.stdin,
       output: process.stdout,
     })
 
-    readline.question(
+    rl.question(
       'Você quer chutar uma letra ou a palavra inteira? (l/p): ',
       (choice) => {
         if (choice.toLowerCase() === 'l') {
-          readline.question('Digite uma letra: ', (letter) => {
-            gameController.guessLetter(letter)
-            readline.close()
-            nextStep()
+          rl.question('Digite uma letra: ', (letter) => {
+            this.guessLetter(letter)
+            rl.close()
+            this.nextStep()
           })
         } else if (choice.toLowerCase() === 'p') {
-          readline.question('Digite a palavra: ', (word) => {
-            gameController.guessWord(word)
-            readline.close()
-            nextStep()
+          rl.question('Digite a palavra: ', (word) => {
+            this.guessWord(word)
+            rl.close()
+            this.nextStep()
           })
         } else {
           console.log(
             'Escolha inválida. Por favor, escolha "l" para chutar uma letra ou "p" para chutar a palavra inteira.'
           )
-          readline.close()
-          nextStep()
+          rl.close()
+          this.nextStep()
         }
       }
     )
-
-    function nextStep() {
-      if (!gameController.match.isGameOver()) {
-        askForAction()
-      } else {
-        const message = gameController.match.isWordGuessed()
-          ? 'Você ganhou!'
-          : 'Você perdeu!'
-        console.log(`${message} Fim do jogo!`)
-      }
-    }
   }
+}
 
-  // Começar o jogo pedindo a primeira ação
-  askForAction()
+function playGame() {
+  const gameController = new GameController()
+  gameController.askForWordAndHint()
 }
 
 // Iniciar o jogo
